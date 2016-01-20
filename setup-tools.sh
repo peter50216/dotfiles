632953c2381f18195766e6a137f85190836f004d
Pi-Hsun Shih <peter50216@gmail.com>
2016-01-20 10:43:54 +0800

    Add color, fix path.

--- setup-tools.sh
+++ setup-tools.sh
@@ -20,7 +20,7 @@ if [[ ! -d ~/.rbenv ]]; then
 fi
 
 LATEST_RUBY2=`~/.rbenv/bin/rbenv install -l | grep -E '^\s+2(\.|[0-9])+$' | tail -1 | tr -d '[[:space:]]'`
-read -t 5 -p "Hit ENTER to skip installing ruby $LATEST_RUBY2 or wait 5 seconds"
+read -t 5 -p $'Hit ENTER to skip installing \e[1;37mruby '"$LATEST_RUBY2"$'\e[m or wait 5 seconds'
 if [[ $? != 0 ]]; then
   # Install latest ruby in rbenv!
   ~/.rbenv/bin/rbenv install $LATEST_RUBY2 &
@@ -33,14 +33,14 @@ if [[ ! -d ~/.pyenv ]]; then
 fi
 
 LATEST_PYTHON2=`~/.pyenv/bin/pyenv install -l | grep -E '^\s+2(\.|[0-9])+$' | tail -1 | tr -d '[[:space:]]'`
-read -t 5 -p "Hit ENTER to skip installing python $LATEST_PYTHON2 or wait 5 seconds"
+read -t 5 -p $'Hit ENTER to skip installing \e[1;37mpython '"$LATEST_PYTHON2"$'\e[m or wait 5 seconds'
 if [[ $? != 0 ]]; then
   # Install latest python in pyenv!
   ~/.pyenv/bin/pyenv install $LATEST_PYTHON2 &
 fi
 
 LATEST_PYTHON3=`~/.pyenv/bin/pyenv install -l | grep -E '^\s+3(\.|[0-9])+$' | tail -1 | tr -d '[[:space:]]'`
-read -t 5 -p "Hit ENTER to skip installing python $LATEST_PYTHON3 or wait 5 seconds"
+read -t 5 -p $'Hit ENTER to skip installing \e[1;37mpython '"$LATEST_PYTHON3"$'\e[m or wait 5 seconds'
 if [[ $? != 0 ]]; then
   # Install latest python in pyenv!
   ~/.pyenv/bin/pyenv install $LATEST_PYTHON3 &
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-read -t 5 -p "Hit ENTER to skip apt-get update or wait 5 seconds" || sudo apt-get update
+read -t 5 -p $"Hit ENTER to skip \e[1;37mapt-get update\e[m or wait 5 seconds" || sudo apt-get update
 
 sudo apt-get -y install build-essential
 sudo apt-get -y install zsh
@@ -14,4 +14,4 @@ sudo apt-get -y install git
 # use zsh! sudoing since we already have used sudo in this shell :P
 sudo chsh -s /bin/zsh $USER
 
-read -t 5 -p "Hit ENTER to skip useful tools setup or wait 5 seconds" || ./setup-tools.sh
+read -t 5 -p $"Hit ENTER to skip \e[1;37museful tools setup\e[m or wait 5 seconds" || $FRESH_LOCAL/setup-tools.sh
