2fc64c4d4a07aa9924fcff2d0610de87edd69a33
Pi-Hsun Shih <peter50216@gmail.com>
2016-01-20 13:17:36 +0800

    One more try!

--- setup/utils.sh
+++ setup/utils.sh
@@ -1,18 +1,18 @@
 function ask_skip {
   local name=$1
   local time=${2:-5}
-  read -t $time -p $'Hit ENTER to skip \e[1;37m'"$name"$'\e[m or wait '"$time"$' seconds'
+  read -t $time -p $'Hit ENTER to skip \e[1;37m'"$name"$'\e[m or wait '"$time"$' seconds\n'
 }
 
 function run_in_tmux {
   local session_name=$1; shift
   local name=$1; shift
   local cmd=$1; shift
-  tmux new-session -d -n "$name" -s "$session_name" "$cmd"
+  tmux new-session -d -n "$name" -s "$session_name" "$cmd || (echo -e '\e[1;33mCommand fail QQ\e[m'; $SHELL)'"
   while [[ $# -ne 0 ]]; do
     name=$1; shift
     cmd=$1; shift
-    tmux new-window -n "$name" "$cmd"
+    tmux new-window -n "$name" "$cmd || (echo -e '\e[1;33mCommand fail QQ\e[m'; $SHELL)'"
   done
   tmux attach-session
 }
