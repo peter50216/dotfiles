375b9c2082d3453d9fd74df00281353bc215cca7
Pi-Hsun Shih <peter50216@gmail.com>
2016-01-21 14:27:12 +0800

    Stupid bug... we have 2 python2...

--- setup/tools.sh
+++ setup/tools.sh
@@ -42,7 +42,7 @@ if ! ask_skip "Python $LATEST_PYTHON2"; then
   cmds+=("Python $LATEST_PYTHON2" "~/.pyenv/bin/pyenv install $LATEST_PYTHON2")
 fi
 
-LATEST_PYTHON3=`~/.pyenv/bin/pyenv install -l | awk '$1~/^2[.0-9]+$/{print $1}' | tail -n 1`
+LATEST_PYTHON3=`~/.pyenv/bin/pyenv install -l | awk '$1~/^3[.0-9]+$/{print $1}' | tail -n 1`
 if ! ask_skip "Python $LATEST_PYTHON3"; then
   # Install latest python in pyenv!
   cmds+=("Python $LATEST_PYTHON3" "~/.pyenv/bin/pyenv install $LATEST_PYTHON3")
