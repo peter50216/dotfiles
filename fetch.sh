142ffd7100c60f794d16578a1e647010099890db
Pi-Hsun Shih <peter50216@gmail.com>
2016-01-25 15:06:33 +0800

    More fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,12 +4,13 @@ if ! command -v apt-get >/dev/null 2>&1; then
   exit 1
 fi
 # XXX(Darkpi): Do we really want this?
-if ! sudo -l; then
+if ! sudo -l >/dev/null 2>&1; then
   echo $'\e[1;31mIt appears that you can\'t sudo, but the script assumes sudo privilege in lots of place...\e[m'
   exit 1
 fi
 if ! command -v git >/dev/null 2>&1; then
   echo $'\e[1;33mgit not installed, installing...\e[m'
+  sudo apt-get update
   sudo apt-get -y install git
 fi
 git clone https://github.com/peter50216/dotfiles ~/dotfiles
