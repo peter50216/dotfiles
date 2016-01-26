e05c092759638c6d6fc1da45c7565c0c9f54567d
Pi-Hsun Shih <peter50216@gmail.com>
2016-01-26 17:17:01 +0800

    Check things before install.

--- setup/essentials.sh
+++ setup/essentials.sh
@@ -4,14 +4,16 @@ set -e
 source setup/utils.sh
 ask_skip 'apt-get update' || sudo apt-get update
 
+# Detect if things are already installed, so that we don't override user
+# compiled ones.
 sudo apt-get -y install build-essential
-sudo apt-get -y install zsh
-sudo apt-get -y install tmux
-sudo apt-get -y install vim
-sudo apt-get -y install cmake
+command -v zsh >/dev/null 2>&1 || sudo apt-get -y install zsh
+command -v tmux >/dev/null 2>&1 || sudo apt-get -y install tmux
+command -v vim >/dev/null 2>&1 || sudo apt-get -y install vim
+command -v cmake >/dev/null 2>&1 || sudo apt-get -y install cmake
 
 # we should already have this :P
-sudo apt-get -y install git
+command -v git >/dev/null 2>&1 || sudo apt-get -y install git
 
 # use zsh! sudoing since we already have used sudo in this shell :P
 sudo chsh -s /bin/zsh $USER
