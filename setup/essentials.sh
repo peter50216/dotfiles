445ba6002530e3f733896bb49ee68cb20e6221de
Pi-Hsun Shih <peter50216@gmail.com>
2016-01-26 11:29:20 +0800

    small fix here and there

--- setup/essentials.sh
+++ setup/essentials.sh
@@ -16,9 +16,9 @@ sudo apt-get -y install git
 # use zsh! sudoing since we already have used sudo in this shell :P
 sudo chsh -s /bin/zsh $USER
 
-# install tmux-mem-cpu-load, shik's fork.
+# install tmux-mem-cpu-load, my fork from shik.
 dir=`mktemp -d`
-git clone https://bitbucket.org/shik/tmux-mem-cpu-load.git "$dir"
+git clone https://bitbucket.org/peter50216/tmux-mem-cpu-load.git "$dir"
 pushd "$dir"
   cmake .
   make
