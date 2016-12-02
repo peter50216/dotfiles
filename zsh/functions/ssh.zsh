d1af0719d66597f669d606299b384c7350120124
Peter Shih <peter50216@gmail.com>
2016-12-02 13:00:54 +0800

    fix some window title change issue in ssh

--- zsh/functions/ssh.zsh
+++ zsh/functions/ssh.zsh
@@ -1,8 +1,13 @@
 ssh() {
   if [ "$(ps -p $(ps -p $$ -o ppid=) -o comm=)" = "tmux" ]; then
-    tmux rename-window -- $(echo "${@[-1]}" | cut -d ' ' -f 1)
+    # This actually gets current active window... (sleep 5; ssh ...) and switch
+    # to another window would change wrong window title... But still this is
+    # better then nothing since at least it would change the window back most of
+    # the time :D
+    local window_num="$(tmux display-message -p "#I")"
+    tmux rename-window -t "${window_num}" -- $(echo "${@[-1]}" | cut -d ' ' -f 1)
     command ssh "$@"
-    tmux set-window-option automatic-rename "on" 1>/dev/null
+    tmux set-window-option -t "${window_num}" automatic-rename "on" 1>/dev/null
   else
     command ssh "$@"
   fi
