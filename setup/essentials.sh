d1624b2026650f3486079b184074e7e52967ce9f
Pi-Hsun Shih <pihsun@chromium.org>
2021-07-02 15:19:23 +0800

    choose name based on host

--- setup/essentials.sh
+++ setup/essentials.sh
@@ -9,10 +9,16 @@ ask_skip 'apt-get update' || sudo apt-get update
 sudo apt-get -y install build-essential
 command -v zsh >/dev/null 2>&1 || sudo apt-get -y install zsh
 command -v tmux >/dev/null 2>&1 || sudo apt-get -y install tmux
-command -v vim >/dev/null 2>&1 || sudo apt-get -y install vim
 command -v cmake >/dev/null 2>&1 || sudo apt-get -y install cmake
 command -v ssh-agent >/dev/null 2>&1 || sudo apt-get -y install keychain
 
 # we should already have this :P
 command -v git >/dev/null 2>&1 || sudo apt-get -y install git
 
+if [ ! -f ~/.gitconfig_local ]; then
+  if grep -q GOOGLE /etc/lsb-release 2>/dev/null; then
+    cp config/gitconfig_defaults/google ~/.gitconfig_local
+  else
+    cp config/gitconfig_defaults/public ~/.gitconfig_local
+  fi
+fi
