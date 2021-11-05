093700ca36ffd52c3b3852e58ac2b13bb949821a
Pi-Hsun Shih <pihsun@chromium.org>
2021-11-05 15:36:01 +0800

    add just

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,3 +7,5 @@ alias reboot='echo "no"'
 
 alias glr="git reflog --format='%C(auto)%h %<(9)%gd %C(blue)%ci%C(reset) %s'"
 alias gcp="git cherry-pick"
+
+alias .j='just --justfile ~/.user.justfile --working-directory .'
