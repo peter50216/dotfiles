62adbceaaaf1b73554ade0ddc4b238598d444b35
Pi-Hsun Shih <pihsun@chromium.org>
2023-03-15 19:01:19 +0800

    fix: missing quote

--- config/nvim/lua/plugins/vim-plugin.lua
+++ config/nvim/lua/plugins/vim-plugin.lua
@@ -1,16 +1,18 @@
 return {
-  -- TODO(Darkpi): The lvimrc support use python3 which slows down startup time
-  -- a LOT (200ms).
-  "peter50216/vim-plugin",
-  config = function()
-    vim.keymap.set("n", "<Leader>occ", "<Plug>CopyCrosURLWithHash", {
-      silent = true,
-      desc = "Copy URL of current (CrOS) file with commit hash from gerrit",
-    })
+  {
+    -- TODO(Darkpi): The lvimrc support use python3 which slows down startup time
+    -- a LOT (200ms).
+    "peter50216/vim-plugin",
+    config = function()
+      vim.keymap.set("n", "<Leader>occ", "<Plug>CopyCrosURLWithHash", {
+        silent = true,
+        desc = "Copy URL of current (CrOS) file with commit hash from gerrit",
+      })
 
-    vim.keymap.set("n", "<Leader>ocm", "<Plug>CopyCrosURL", {
-      silent = true,
-      desc = "Copy URL of current (CrOS) file from gerrit",
-    })
-  end,
+      vim.keymap.set("n", "<Leader>ocm", "<Plug>CopyCrosURL", {
+        silent = true,
+        desc = "Copy URL of current (CrOS) file from gerrit",
+      })
+    end,
+  },
 }
