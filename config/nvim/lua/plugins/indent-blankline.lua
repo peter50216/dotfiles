a07f9d588c18ba556acb0acc6b2a461f237ce5fe
Pi-Hsun Shih <pihsun@chromium.org>
2023-10-25 17:04:04 +0800

    fix: configure indent-blankline correctly

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -3,33 +3,24 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     config = function(_)
-      local au_id = vim.api.nvim_create_augroup("autocmd_indent_blankline", {})
-      vim.api.nvim_create_autocmd("Colorscheme", {
-        group = au_id,
-        callback = function()
-          -- vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", {
-          -- bg = "#141414",
-          -- })
-          -- vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", {
-          -- fg = "#64748b",
-          -- })
-          -- vim.api.nvim_set_hl(0, "IndentBlanklineContextStart", {
-          -- sp = "#64748b",
-          -- underline = true,
-          -- })
-        end,
-      })
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", {
+          fg = "#101810",
+        })
+        vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", {
+          fg = "#101020",
+        })
+      end)
       require("ibl").setup {
         scope = {
-          enabled = true,
-          show_start = true,
+          enabled = false,
         },
         indent = {
-          char = "",
-          -- highlight = {
-          -- "IndentBlanklineIndent1",
-          -- "IndentBlanklineIndent2",
-          -- },
+          highlight = {
+            "IndentBlanklineIndent1",
+            "IndentBlanklineIndent2",
+          },
         },
         exclude = {
           filetypes = {
