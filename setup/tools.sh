020a379a1b6661df7cb07475f429cc8e44547cdd
Pi-Hsun Shih <pihsun@chromium.org>
2023-10-25 16:47:56 +0800

    update

--- setup/tools.sh
+++ setup/tools.sh
@@ -6,12 +6,11 @@ source setup/utils.sh
 sudo apt-get -y install aptitude
 sudo apt-get -y install htop
 sudo apt-get -y install keychain
-sudo apt-get -y install python-setuptools
-sudo apt-get -y install python-dev
+sudo apt-get -y install python3-dev
 sudo apt-get -y install shellcheck
 
 # for building python / ruby
-sudo apt-get -y install libssl-dev libreadline-dev libbz2-dev libsqlite3-dev libffi-dev zlib1g-dev wget curl llvm libncurses5-dev libncursesw5-dev libyaml-dev
+sudo apt-get -y install libssl-dev libreadline-dev libbz2-dev libsqlite3-dev libffi-dev zlib1g-dev wget curl llvm libncurses-dev libyaml-dev
 
 if [[ -n "$TMUX" ]]; then
   echo "Already in tmux! exit tmux to install rubies / pythons"
