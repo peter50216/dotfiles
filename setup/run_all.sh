741920fd2e7772da333d1351ab03b889e5fdbf2f
Peter Shih <peter50216@gmail.com>
2024-02-16 02:40:49 +0800

    chore: speed up a bit zsh startup

--- setup/run_all.sh
+++ setup/run_all.sh
@@ -4,4 +4,4 @@ set -e
 source setup/utils.sh
 
 ./setup/essentials.sh
-ask_skip 'useful tools setup' || ./setup/tools.sh
+./setup/tools.sh
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,3 +1,5 @@
 export PATH=$HOME/bin:$HOME/bin/common:"$HOME/bin/common/$(uname -m)":$PATH
 
+DO_NOT_TRACK=1
+
 fpath=(~/dotfiles/zsh/completions $fpath)
