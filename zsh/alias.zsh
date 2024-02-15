4d343d17ef8f34d790fd8f200e975a2055012ca1
Peter Shih <peter50216@gmail.com>
2024-02-16 02:40:49 +0800

    chore: speed up a bit zsh startup

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -15,3 +15,18 @@ alias fd='noglob fd'
 
 alias vi='nvim'
 alias ls='eza'
+
+alias catp='bat -P'
+alias cat='bat'
+alias cd='z'
+
+# From rsync prezto module, without the version detection to be much faster.
+_rsync_cmd='rsync --verbose --progress --human-readable --compress --archive \
+  --hard-links --one-file-system --acls --xattrs'
+
+alias rsync-copy="${_rsync_cmd}"
+alias rsync-move="${_rsync_cmd} --remove-source-files"
+alias rsync-update="${_rsync_cmd} --update"
+alias rsync-synchronize="${_rsync_cmd} --update --delete"
+
+unset _rsync_cmd
