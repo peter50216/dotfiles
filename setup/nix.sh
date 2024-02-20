0b320a941068dc93ed35c7872953822c6e703a40
Peter Shih <peter50216@gmail.com>
2024-02-20 14:11:16 +0800

    fix: nix installer is not reentrant T_T

--- setup/nix.sh
+++ setup/nix.sh
@@ -3,6 +3,9 @@ set -e
 
 ./setup/nix_config.sh
 
-sh <(curl -L https://nixos.org/nix/install) --daemon --yes
-source /etc/profile.d/nix.sh
+if ! command -v nix >/dev/null; then
+  sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+  source /etc/profile.d/nix.sh
+fi
+
 nix run home-manager/master -- init --switch --impure
