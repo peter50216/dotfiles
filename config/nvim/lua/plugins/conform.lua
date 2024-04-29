9f174e76eaddff8411d01c5ee030965720c6dbfb
Pi-Hsun Shih <pihsun@chromium.org>
2024-04-29 17:38:11 +0800

    Add autoformat enable/disable, simplify neo-tree

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
+    cmd = { "ConformInfo", "FormatEnable", "FormatDisable" },
     keys = {
       {
         -- Customize or remove this keymap to your liking
@@ -41,6 +41,25 @@ return {
             vim.opt_local.formatexpr = 'v:lua.require("conform").formatexpr()'
           end
         end,
+      })
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
       })
 
       local slow_format_filetypes = {}
@@ -97,6 +116,9 @@ return {
         },
 
         format_on_save = function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
           if slow_format_filetypes[vim.bo[bufnr].filetype] then
             return
           end
@@ -115,6 +137,9 @@ return {
         end,
 
         format_after_save = function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
           if not slow_format_filetypes[vim.bo[bufnr].filetype] then
             return
           end
