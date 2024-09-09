91f09fc1721a2ae5d244d4d6bc0a2b866f9fd7d1
Peter Shih <peter50216@gmail.com>
2024-09-10 00:51:09 +0800

    add missing dependency of xz

--- setup/install.sh
+++ setup/install.sh
@@ -4,6 +4,12 @@ set -e
 # Minimal setup for nix.
 # Other one-time setup should be using home-manager.
 
+# Nix installer needs xz.
+if ! command -v xz >/dev/null 2>&1; then
+  echo $'\e[1;33mxz not installed, installing...\e[m'
+  sudo apt-get -y install xz-utils
+fi
+
 # Generate default per-host config
 sed "s#USERNAME#$USER#g; s#HOME_DIRECTORY#$HOME#g" ./template/home.nix > ./home.nix
 cp ./template/local.nix ./local.nix
