a60f610d8b60e34d943bbfa652f7efe88b0b014e
Peter Shih <peter50216@gmail.com>
2024-09-09 22:03:33 +0800

    move git config to nix

---
+++ home-manager/config/default.nix
@@ -0,0 +1,6 @@
+{...}: {
+  imports = [
+    ./git.nix
+    ./tmux.nix
+  ];
+}
--- setup/nix_config.sh
+++ setup/nix_config.sh
@@ -2,4 +2,4 @@
 set -e
 
 sed "s#USERNAME#$USER#g; s#HOME_DIRECTORY#$HOME#g; s#SYSTEM#$(uname -m)-linux#g" ./home-manager/template/home.nix > ./home-manager/home.nix
-sed "s#USERNAME#$USER#g; s#HOME_DIRECTORY#$HOME#g; s#SYSTEM#$(uname -m)-linux#g" ./home-manager/template/extra.nix > ~/home-manager/extra.nix
+sed "s#USERNAME#$USER#g; s#HOME_DIRECTORY#$HOME#g; s#SYSTEM#$(uname -m)-linux#g" ./home-manager/template/local.nix > ./home-manager/local.nix
