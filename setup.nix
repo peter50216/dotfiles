2484ea3ea326865c1c2516ab68c1e3f977b2436b
Peter Shih <peter50216@gmail.com>
2026-04-14 18:52:08 +0800

    refactor: make setup activation idempotent

--- setup.nix
+++ setup.nix
@@ -15,27 +15,22 @@
       fi
     '';
 
-    runSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      if [[ -f "$HOME/.setup-done" ]]; then
-        verboseEcho "Setup has already been run. Skipping..."
-      else
-        verboseEcho "Running one-time setup..."
-
-        if [ ! -f ~/.gitconfig ]; then
-          if grep -q GOOGLE /etc/lsb-release 2>/dev/null; then
-            run cp ~/dotfiles/external/gitconfig_defaults/google ~/.gitconfig
-          else
-            run cp ~/dotfiles/external/gitconfig_defaults/public ~/.gitconfig
-          fi
+    seedGitconfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      if [[ ! -f "$HOME/.gitconfig" ]]; then
+        verboseEcho "Seeding ~/.gitconfig from dotfiles defaults..."
+        if grep -q GOOGLE /etc/lsb-release 2>/dev/null; then
+          run cp ~/dotfiles/external/gitconfig_defaults/google ~/.gitconfig
+        else
+          run cp ~/dotfiles/external/gitconfig_defaults/public ~/.gitconfig
         fi
+      fi
+    '';
 
-        if [ ! -f "$HOME/.config/mise/config.toml" ]; then
-          run mkdir -p "$HOME/.config/mise"
-          run cp ~/dotfiles/template/mise-config.toml "$HOME/.config/mise/config.toml"
-        fi
-
-        run touch "$HOME/.setup-done"
-        verboseEcho "Setup completed successfully!"
+    seedMiseConfig = lib.hm.dag.entryAfter ["migrateMiseConfig"] ''
+      if [[ ! -f "$HOME/.config/mise/config.toml" ]]; then
+        verboseEcho "Seeding ~/.config/mise/config.toml from dotfiles template..."
+        run mkdir -p "$HOME/.config/mise"
+        run cp ~/dotfiles/template/mise-config.toml "$HOME/.config/mise/config.toml"
       fi
     '';
     # TODO: Somehow automate chsh zsh
