7e400a773b2d11b011316c4b13b2b6c0a2f1fcb9
Peter Shih <peter50216@gmail.com>
2026-04-21 18:44:42 +0800

    refactor: drop custom zsh session vars hook

--- zsh/base.nix
+++ zsh/base.nix
@@ -1,17 +1,13 @@
-let
-  hmSessionVars = builtins.readFile ./hm-session-vars.zsh;
-in {
+{
   programs.zsh = {
     enable = true;
 
     # TODO(Darkpi): Some way to simplify this?
     initContent = ''
-      ${hmSessionVars}
       ${builtins.readFile ./functions.zsh}
       ${builtins.readFile ./init.zsh}
     '';
     profileExtra = ''
-      ${hmSessionVars}
       ${builtins.readFile ./profile.zsh}
     '';
 
